17ca82a876294962c74975767a225310b913076e
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-06-04 00:31:41 +0200

    Update SL_tasks.py

--- SL_tasks.py
+++ SL_tasks.py
@@ -19,7 +19,7 @@ jtbd_task = Task(
             <h2>- A title</h2>
             <h2>- An explanation of what JTBD are and why it is essential to identify them</h2>
             <h2>- At least 8 JTBD identified through the simulated interview, each described in detail, 4-5 of these should be related to the product or service and the rest can be outside the scope of the offering but are still important to the customer</h2>
-            <p>Use headings, subheadings, bullet points, and numbered lists to structure the report in an easy-to-read format.</p>
+            <p>Format the document as a concise, structured report with headings, subheadings, and bullet points for easy reference and sharing among team members.</p>
             <p>Explain how gathering this information through interviews helps businesses:</p>
             <p>- Refine their product offering</p>
             <p>- Target the right audience</p>
@@ -34,6 +34,7 @@ pains_task = Task(
             <h2>- Specific instances of negative experiences customers have had with competing solutions, including direct quotes from customer feedback or focus group discussions</h2>
             <h2>- Potential objections to current or proposed offerings based on trends observed in customer feedback and market research</h2>
             <p>Pains should be related to the jobs-to-be-done (JTBD)</p>
-            <p>Each section should begin with an overview followed by detailed examples. Include headings, sub-headings, bullet points, and numbered lists to structure the report in an easy-to-read format.</p>
+            <p>Each section should begin with an overview followed by detailed examples. </p>
+            <p>Format the document as a concise, structured report with headings, subheadings, and bullet points for easy reference and sharing among team members.</p>
             <p>Conclude the report with a summary that highlights key findings and implications for strategy.</p>"""
 )
