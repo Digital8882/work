7b039513a93ef5f40c470c651b93b66718703166
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-06-03 07:59:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ SENDER_EMAIL = 'info@swiftlaunch.biz'
 SENDER_PASSWORD = 'Lovelife1#'
 
 os.environ["LANGSMITH_TRACING_V2"] = "true"
-os.environ["LANGSMITH_PROJECT"] = "SLwork6"
+os.environ["LANGSMITH_PROJECT"] = "SLwork7"
 os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGSMITH_API_KEY"] = "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"
 
@@ -83,7 +83,7 @@ def start_crew_process(email, product_service, price, currency, payment_frequenc
     project_crew = Crew(
         tasks=[new_task, icp_task, jtbd_task, pains_task],
         agents=[researcher, report_writer],
-        manager_llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        manager_llm=ChatOpenAI(temperature=0, model="gpt-4o"),
         max_rpm=8,
         process=Process.hierarchical,
         memory=True,
