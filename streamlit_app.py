b14e42cfb5cc6fb570959b29c6f9f4d094120aba
HAJAR FASIL <157843888+Digital8882@users.noreply.github.com>
2024-06-03 07:37:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+import json
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
